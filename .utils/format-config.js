583c5a310cd0c4da386ab491d3cc3062eb5ebb89
Mark Wissink <markcwissink@gmail.com>
2023-12-22 18:17:18 -0500

    Update format config

--- .utils/format-config.js
+++ .utils/format-config.js
@@ -1,7 +1,28 @@
+const formatKeyValue = (key, value) => {
+  switch (typeof value) {
+    case 'number':
+    case 'string':
+      return `${key}=${value}`;
+    case 'boolean':
+      return value ? key : '';
+    default:
+      console.error(`Unsupported value type [${typeof value}] for key [${key}]`)
+      process.exit(1);
+  }
+}
+
 import(`${process.cwd()}/${process.argv[2]}`)
   .then((module) => {
-    process.stdout.write(
+    console.log(
       Object.entries(module.default.config)
-        .reduce((acc, [key, value]) => `${acc}\n${key}=${value}`, '')
+        .reduce((acc, [key, value]) => {
+          if (Array.isArray(value)) {
+            value.forEach((arrayValue) => acc.push(formatKeyValue(key, arryaValue)));
+          } else {
+            acc.push(formatKeyValue(key, value));
+          }
+          return acc;
+        }, [])
+        .join('\n')
     );
   });
