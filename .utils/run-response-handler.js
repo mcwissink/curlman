5274aa3244f9a207870ef45fcc34b09f9f61a790
Mark Wissink <markcwissink@gmail.com>
2023-12-22 18:25:26 -0500

    Always log response

--- .utils/run-response-handler.js
+++ .utils/run-response-handler.js
@@ -3,36 +3,37 @@ import readline from 'readline';
 
 const readStdin = () => new Promise((resolve) => {
   let acc = '';
-    readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false,
+  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  })
+    .on('line', (line) =>{
+      acc += `${line}\n`;
     })
-        .on('line', (line) =>{
-            acc += `${line}\n`;
-        })
-        .once('close', () => {
-            resolve(acc);
-        });
+    .once('close', () => {
+      resolve(acc);
+    });
 });
 
 const loadSession = () => {
-    try {
-        const session = fs.readFileSync('.session.json');
-        return new Map(Object.entries(JSON.parse(session)));
-    } catch {
-        return new Map();
-    }
+  try {
+    const session = fs.readFileSync('.session.json');
+    return new Map(Object.entries(JSON.parse(session)));
+  } catch {
+    return new Map();
+  }
 };
 
 import(`${process.cwd()}/${process.argv[2]}`)
-    .then((module) => {
-        readStdin().then((response) => {
-            const session = loadSession();
-            module.default.responseHandler?.({
-                response,
-                session,
-            });
-            fs.writeFileSync('.session.json', JSON.stringify(Object.fromEntries(session)))
-        });
+  .then((module) => {
+    readStdin().then((response) => {
+      console.log(response)
+      const session = loadSession();
+      module.default.responseHandler?.({
+        response,
+        session,
+      });
+      fs.writeFileSync('.session.json', JSON.stringify(Object.fromEntries(session)))
     });
+  });
--- curlman.sh
+++ curlman.sh
@@ -1,3 +1,3 @@
 env $(node .utils/format-json.js .session.json | cat - $1) node .utils/format-config.js $2 \
-     | curl -av -K - \
-     | node .utils/run-response-handler.js $2
+    | curl -av -K - \
+    | node .utils/run-response-handler.js $2
